eda9033028814b658dd424b18af90572085f45e2
Andrew Poe <apoe@decisiv.net>
2017-10-28 15:55:13 -0400

    adds link to the GH  repo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,8 +4,17 @@ import QuestionCard from 'compositions/QuestionCard';
 
 function Home() {
   return (
-    <div className="page">
-      <QuestionCard />
+    <div>
+      <div className="content">
+        <QuestionCard />
+      </div>
+      <div className="content">
+        <div className="content__link">
+          <a href="https://github.com/andrewtpoe/request-answers">
+            View on GitHub
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
